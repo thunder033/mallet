9e83c40f1256c184647336771650669ba4709a56
Greg Rozmarynowycz <crashtest1@live.com>
2017-11-28 21:29:38 -0800

    delete shader source

--- src/mallet/webgl/webgl.module.ts
+++ src/mallet/webgl/webgl.module.ts
@@ -5,10 +5,8 @@ import {MDT} from '../mallet.depedency-tree';
 import {webGLStageOptions} from './webgl-stage.component';
 import {WebGLStage} from './webgl-stage';
 import {ngAnnotate} from '../lib/injector-plus';
-import {ShaderSourceProvider} from './shader-source.provider';
 
 export const malletWebGL = angular.module('mallet.webgl', [mallet.name]);
 
-malletWebGL.provider(MDT.webgl.ShaderSource, ShaderSourceProvider);
 malletWebGL.service(MDT.webgl.WebGLStage, ngAnnotate(WebGLStage));
 malletWebGL.component(MDT.component.webGLStage, webGLStageOptions);
