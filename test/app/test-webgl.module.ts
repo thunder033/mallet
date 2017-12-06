474cbc241479ef02e5457f001fd71aedf95d1d8a
Greg Rozmarynowycz <crashtest1@live.com>
2017-12-05 17:59:02 -0800

    update test app

--- test/app/test-webgl.module.ts
+++ test/app/test-webgl.module.ts
@@ -1,38 +1,86 @@
 import * as angular from 'angular';
-import {IAugmentedJQuery, IController} from 'angular';
+import {IController} from 'angular';
 import {
-    inject,
-    IWebGLStage,
+    Camera,
+    Entity,
+    ICamera,
+    ILibraryService,
+    IRenderer,
+    IShaderOptions,
+    IWebGLResourceContext,
     Level,
-    Logger,
     malletWebGL,
-    MDT,
     ngAnnotate,
-    Scheduler,
+    Renderer,
+    ShaderDTO,
+    WebGLApp,
 } from 'mallet';
+
+class ColorCube extends Entity {
+    private cubeZ: number;
+    private cubeDelta: number;
+    private cubeRot: number;
+
+    constructor(context: IWebGLResourceContext) {
+        super(context, 'cube');
+        this.cubeZ = -1;
+        this.cubeDelta = 1 / 500;
+        this.cubeRot = 0;
+    }
+
+    public update(dt: number, tt: number): void {
+        this.cubeZ += dt * this.cubeDelta;
+        this.cubeRot += dt * this.cubeDelta * 200;
+
+        const min = -10;
+        const max = -0.1 - 0.5;
+        if (this.cubeZ < min || this.cubeZ > max) {
+            this.cubeZ = Math.min(min, Math.max(this.cubeZ, max));
+            this.cubeDelta *= -1;
+        }
+
+        this.transform.setPosition(.25, -.15, this.cubeZ);
+        this.transform.setRotation(this.cubeRot, this.cubeRot, this.cubeRot);
+    }
+}
 
 const testApp = angular.module('mallet-test', [malletWebGL.name]);
 
 /**
  * Test Harness to demonstrate basic functionality of the Mallet engine
  */
-class TestCtrl implements IController {
+class TestApp extends WebGLApp implements IController {
+    protected camera: ICamera;
+    protected renderer: IRenderer;
 
-    constructor(
-        @inject(MDT.webgl.WebGLStage) private stage: IWebGLStage,
-        @inject(MDT.Scheduler) private scheduler: Scheduler,
-        @inject(MDT.ng.$element) private $element: IAugmentedJQuery,
-        @inject(MDT.Logger) private logger: Logger) {
-        logger.info('Running Test Controller');
+    public config() {
         this.logger.config({level: Level.Debug});
+        this.logger.info('Running Test Controller');
     }
 
-    public $postLink(): void {
-        this.scheduler.startMainLoop();
+    public init({renderTarget}: IWebGLResourceContext): any {
+        return Promise.all([ // load resources
+            this.library.get(ShaderDTO, '3d-vertex-shader'),
+            this.library.get(ShaderDTO, 'fragment-shader'),
+        ]).then(([vertex, fragment]: [IShaderOptions, IShaderOptions]) => {
+            const program = this.stage.addProgram({shaders: {vertex, fragment}, name: '3d'});
+            const glFactory = this.stage.getFactory();
+
+            this.camera = new Camera(renderTarget.getAspectRatio());
+            this.renderer = glFactory.create(Renderer, {camera: this.camera, program});
+            this.postUpdate = this.camera.update; // cut out some overhead by just assigning it for now
+
+            // setup lighting
+            program.getUniformSetter('light.ambientColor')(0.1, 0.1, 0.1, 1.0);
+            program.getUniformSetter('light.diffuseColor')(0.8, 0.8, 0.8, 1.0);
+            program.getUniformSetter('light.direction')(-1, 0, 0);
+
+            glFactory.create(ColorCube);
+        });
     }
 }
 
 testApp.component('testHarness', {
-    controller: ngAnnotate(TestCtrl) as any,
+    controller: ngAnnotate(TestApp, WebGLApp) as any,
     template: '<mallet-webgl-stage></mallet-webgl-stage>',
 });
