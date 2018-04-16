ed26c0cd5601c55f21a4543af83a136ab31f5135
Greg Rozmarynowycz <crashtest1@live.com>
2018-04-16 00:40:31 -0700

    better state-machine toString, fix removeState, add reverse-look up states

--- src/mallet/lib/state-machine.ts
+++ src/mallet/lib/state-machine.ts
@@ -22,24 +22,42 @@ class StateListener {
     }
 }
 
+/**
+ * Property decorator to define a state-machine state. Defines both forward and revers lookup properties
+ * @param {StateMachine} target
+ * @param {string} key
+ */
 export function state(target: Object, key: string) {
+    // ensure the property can be defined on the target
     if (delete target[key]) {
+        const value = Math.pow(2, Object.keys(target).length);
+        // define regular lookup value
         Object.defineProperty(target, key, {
             enumerable: true,
-            value: Math.pow(2, Object.keys(target).length),
+            value,
         });
+        // define non-enumerable reverse-lookup
+        Object.defineProperty(target, value, {value: key});
     }
 }
 
 /* tslint:disable:no-shadowed-variable */
+/**
+ * State machine implementation that with compound-state functionality
+ */
 export abstract class StateMachine {
 
     @enumerable(false)
-    private state;
+    private state: number;
 
     @enumerable(false)
     private stateListeners: StateListener[];
 
+    /**
+     * Get value that represents all possible states for the machine
+     * @param {{new(): StateMachine}} machine
+     * @returns {number}
+     */
     public static all(machine: {new(): StateMachine}): number {
         return Object.keys(machine).reduce((all: number, state: string) => {
             return all | machine[state];
@@ -56,10 +74,14 @@ export abstract class StateMachine {
      * @param state
      * @returns {boolean}
      */
-    public is(state) {
+    public is(state): boolean {
         return (state | this.state) === this.state;
     }
 
+    /**
+     * Get the current state value
+     * @returns {number}
+     */
     public getState() {
         return this.state;
     }
@@ -80,7 +102,11 @@ export abstract class StateMachine {
         this.stateListeners.length = 0;
     }
 
-    public setState(state) {
+    /**
+     * Set the state to the new value (does not add compound state)
+     * @param state
+     */
+    public setState(state: number) {
         const prevState = this.state;
         this.state = state;
         if (prevState !== this.state) {
@@ -88,6 +114,10 @@ export abstract class StateMachine {
         }
     }
 
+    /**
+     * Add a state value to the current compound state
+     * @param {number} state
+     */
     public addState(state): void {
         const prevState = this.state;
         this.state |= state;
@@ -96,18 +126,44 @@ export abstract class StateMachine {
         }
     }
 
+    /**
+     * Remove all values from the compound state
+     */
     public reset(): void {
         this.state = 0;
     }
 
-    public removeState(state) {
+    /**
+     * Remove a state from the current compound state
+     * @param {number} state
+     */
+    public removeState(state: number) {
         const prevState = this.state;
-        this.state ^= state;
+        this.state ^= (this.state & state);
         if (prevState !== this.state) {
             this.invokeStateListeners(this.state, prevState);
         }
     }
 
+    /**
+     * Override the to string method to output the current state value (with full state name)
+     * @returns {string}
+     */
+    public toString() {
+        const states = this.state.toString(2)
+            .split('')
+            // the @state decorator assigned lookup values to the class
+            .map((value, place) => parseInt(value, 10) > 0 ? this.constructor[2 ** place] : '')
+            .filter((stateName) => stateName)
+            .join(', ');
+        return `[${this.constructor.name}: ${this.state} = ${states}]`;
+    }
+
+    /**
+     * Invoke listeners listening for the given state
+     * @param {number} state
+     * @param {number} prevState
+     */
     private invokeStateListeners(state: number, prevState: number) {
         this.stateListeners.forEach((listener) => {
             if ((listener.getState() | state) === state) {
